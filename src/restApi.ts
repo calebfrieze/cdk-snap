6d40a7c45c12c97693effb6fe03e0c126b39166f
Caleb Frieze <calebfrieze@gmail.com>
2024-07-15 13:52:27 -0700

    fix: conflict

--- src/restApi.ts
+++ src/restApi.ts
@@ -86,14 +86,6 @@ const attachResources = (
 					new LambdaIntegration(method.function)
 				);
 			}
-
-		if (resource.methods?.length) {
-			for (let method of resource.methods) {
-				apiResource.addMethod(
-					method.verb,
-					new LambdaIntegration(method.function)
-				);
-			}
 		}
 	}
 };
